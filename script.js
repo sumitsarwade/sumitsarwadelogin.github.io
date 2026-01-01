ef4d9bd0ede6fe52d06761b5339b0882923bcdd4
Sumit <112116689+sumitsarwade@users.noreply.github.com>
2026-01-02 00:55:45 +0530

    Modify content message in script.js
    Updated the message in content .

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 const content = {
   date: "02 Jan 2026",
   title: "Daily Focus",
-  message: "Missing Me"
+  message: "Missing Me My Everything"
 };
 
 // Render
 document.getElementById("date").innerText = content.date;
 document.getElementById("title").innerText = content.title;
 document.getElementById("message").innerText = content.message.trim();
+
