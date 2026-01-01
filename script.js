de2670d8c520e993d9e8192b5eb0e75d47878bd6
Sumit <112116689+sumitsarwade@users.noreply.github.com>
2026-01-02 00:53:04 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+const content = {
+  date: "02 Jan 2026",
+  title: "Daily Focus",
+  message: "Missing Me"
+};
+
+// Render
+document.getElementById("date").innerText = content.date;
+document.getElementById("title").innerText = content.title;
+document.getElementById("message").innerText = content.message.trim();
